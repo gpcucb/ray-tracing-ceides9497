1576c5206c86fe37ef4f580dd74972803398358b
César Ivan Delgado Silva <ceides9497@gmail.com>
2017-04-04 15:11:46 -0400

    Mi paisaje 1

--- raytracer.rb
+++ raytracer.rb
@@ -18,8 +18,8 @@ class RayTracer < Renderer
     @nx = @width
     @ny = @height
     # Camera values
-    e= Vector.new(0,0,-200)
-    center= Vector.new(0,0,-100)
+    e= Vector.new(0,50,-200)
+    center= Vector.new(0,50,-100)
     up= Vector.new(0,1,0)
     fov= 39.31
     df=0.035
@@ -32,60 +32,106 @@ class RayTracer < Renderer
 
     #colors
     mediumBlue = Rgb.new(0.196078,0.196078,0.8)
+    fucsia = Rgb.new(1.0, 0.0, 1.0)
+    orange = Rgb.new(1, 0.5,0.0)
+    pink = Rgb.new(0.737255 , 0.560784 , 0.560784)
+    skyBlue = Rgb.new(0.196078,0.6,0.8)
+    silver = Rgb.new(0.90,0.91,0.98)
+    yellow = Rgb.new(1.0, 1.0, 0)
+    seaGreen = Rgb.new(0.137255,0.556863,0.419608)
 
     # Sphere values
-    position = Vector.new(0,60,300)
-    radius = 50
-    sphere_diffuse = Rgb.new(1.0, 0.0, 1.0)
+    position = Vector.new(0,25,300)
+    radius = 25
+    sphere_diffuse = fucsia
     sphere_specular =Rgb.new(1.0,1.0,1.0)
     sphere_reflection = 0.5
     sphere_power = 60
     sphere_material = Material.new(sphere_diffuse, sphere_reflection, sphere_specular, sphere_power)
 
-    # Triangle values
-    a = Vector.new(220,0,360)
-    b = Vector.new(-220,0,360)
-    c = Vector.new(220,100,360)
-    triangle_diffuse = Rgb.new(1.0, 1.0, 0)
+    position2 = Vector.new(50,25,300)
+    radius2 = 25
+    sphere_diffuse2 = seaGreen
+    sphere_specular2 =Rgb.new(1.0,1.0,1.0)
+    sphere_reflection2 = 0.5
+    sphere_power2 = 60
+    sphere_material2 = Material.new(sphere_diffuse2, sphere_reflection2, sphere_specular2, sphere_power2)
+
+    position3 = Vector.new(-50,25,300)
+    radius3 = 25
+    sphere_diffuse3 = yellow
+    sphere_specular3 =Rgb.new(1.0,1.0,1.0)
+    sphere_reflection3 = 0.5
+    sphere_power3 = 60
+    sphere_material3 = Material.new(sphere_diffuse3, sphere_reflection3, sphere_specular3, sphere_power3)
+
+    # Triangle values - walls
+    a = Vector.new(220,0,325)
+    b = Vector.new(0,0,325)
+    c = Vector.new(220,150,310)
+    triangle_diffuse = silver
     triangle_specular = Rgb.new(1.0,1.0,1.0)
     triangle_reflection = 0.5
     triangle_power = 60
     triangle_material = Material.new(triangle_diffuse, triangle_reflection, triangle_specular, triangle_power)
 
-    a2 = Vector.new(-220,100,360)
-    b2 = Vector.new(220,100,360)
-    c2 = Vector.new(-220,0,360)
-    triangle_diffuse2 = Rgb.new(1.0, 1.0, 0)
+    a2 = Vector.new(0,150,325)
+    b2 = Vector.new(220,150,310)
+    c2 = Vector.new(0,0,325)
+    triangle_diffuse2 = silver
     triangle_specular2 = Rgb.new(1.0,1.0,1.0)
     triangle_reflection2 = 0.5
     triangle_power2 = 60
     triangle_material2 = Material.new(triangle_diffuse2, triangle_reflection2, triangle_specular2, triangle_power2)
 
-    a3 = Vector.new(220,0,360)
-    b3 = Vector.new(100,-25,160)
-    c3 = Vector.new(-220,0,360)
-    triangle_diffuse3 = mediumBlue
-    triangle_specular3 = Rgb.new(1.0,1.0,1.0)
-    triangle_reflection3 = 0.5
-    triangle_power3 = 60
-    triangle_material3 = Material.new(triangle_diffuse3, triangle_reflection3, triangle_specular3, triangle_power3)
+    aw = Vector.new(0,0,325)
+    bw = Vector.new(-220,0,325)
+    cw = Vector.new(-220,150,310)
+    triangle_diffusew = silver
+    triangle_specularw = Rgb.new(1.0,1.0,1.0)
+    triangle_reflectionw = 0.5
+    triangle_powerw = 60
+    triangle_materialw = Material.new(triangle_diffusew, triangle_reflectionw, triangle_specularw, triangle_powerw)
 
-    a4 = Vector.new(-220,0,360)
-    b4 = Vector.new(100,-25,160)
-    c4 = Vector.new(-100,-25,160)
-    triangle_diffuse4 = mediumBlue
-    triangle_specular4 = Rgb.new(1.0,1.0,1.0)
-    triangle_reflection4 = 0.5
-    triangle_power4 = 60
-    triangle_material4 = Material.new(triangle_diffuse4, triangle_reflection4, triangle_specular4, triangle_power4)
+    aw1 = Vector.new(-220,150,310)
+    bw1 = Vector.new(0,150,325)
+    cw1 = Vector.new(0,0,325)
+    triangle_diffusew1 = silver
+    triangle_specularw1 = Rgb.new(1.0,1.0,1.0)
+    triangle_reflectionw1 = 0.5
+    triangle_powerw1 = 60
+    triangle_materialw1 = Material.new(triangle_diffusew1, triangle_reflectionw1, triangle_specularw1, triangle_powerw1)
+
+    #piso
+    ap = Vector.new(220,0,325)
+    bp = Vector.new(100,0,160)
+    cp = Vector.new(-220,0,325)
+    triangle_diffusep = mediumBlue
+    triangle_specularp = Rgb.new(1.0,1.0,1.0)
+    triangle_reflectionp = 0.5
+    triangle_powerp = 60
+    triangle_materialp = Material.new(triangle_diffusep, triangle_reflectionp, triangle_specularp, triangle_powerp)
+
+    ap1 = Vector.new(-220,0,325)
+    bp1 = Vector.new(100,0,160)
+    cp1 = Vector.new(-100,0,160)
+    triangle_diffusep1 = mediumBlue
+    triangle_specularp1 = Rgb.new(1.0,1.0,1.0)
+    triangle_reflectionp1 = 0.5
+    triangle_powerp1 = 60
+    triangle_materialp1 = Material.new(triangle_diffusep1, triangle_reflectionp1, triangle_specularp1, triangle_powerp1)
 
     @sphere = Sphere.new(position, radius, sphere_material)
+    @sphere2 = Sphere.new(position2, radius2, sphere_material2)
+    @sphere3 = Sphere.new(position3, radius3, sphere_material3)
     @triangle = Triangle.new(a, b, c, triangle_material)
     @triangle2 = Triangle.new(a2, b2, c2, triangle_material2)
-    @triangle3 = Triangle.new(a3, b3, c3, triangle_material3)
-    @triangle4 = Triangle.new(a4, b4, c4, triangle_material4)
+    @trianglew = Triangle.new(aw, bw, cw, triangle_materialw)
+    @trianglew1 = Triangle.new(aw1, bw1, cw1, triangle_materialw1)
+    @trianglep = Triangle.new(ap, bp, cp, triangle_materialp)
+    @trianglep1 = Triangle.new(ap1, bp1, cp1, triangle_materialp1)
     @objects=[]
-    @objects << @triangle4 << @triangle3 << @triangle2 << @triangle << @sphere
+    @objects << @trianglep1 << @trianglep << @trianglew1 << @trianglew << @triangle2 << @triangle << @sphere << @sphere2 << @sphere3
   end
 
   def max(number1,number2)
@@ -121,7 +167,7 @@ class RayTracer < Renderer
   end
 
   def ambientShading(object)
-    @ambientLight = Rgb.new(0.8,0.7,0.6)
+    @ambientLight = Rgb.new(0.003,0.003,0.003)
     ka = object.material.diffuse
     return @ambientLight.multiply_color(ka)
   end
